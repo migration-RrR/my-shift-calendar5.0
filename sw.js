6f146d6d8c7a1c2434159ff154d85069dba689c4
Kizaru_010 <figmamoon@gmail.com>
2026-03-15 17:06:19 +0500

    Refactor service worker for caching strategy

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("calendar-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js"
-      ]);
-    })
+const CACHE_NAME = "calendar-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
